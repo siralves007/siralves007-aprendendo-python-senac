ea95c873f833cc39f1bf7163367a67b4a62e6931
Matheus Alves Santos <matheus.alves7@hotmail.com>
2025-08-13 18:39:44 -0300

    Aula teste para rodar java no VScode

---
+++ teste01.js
@@ -0,0 +1,15 @@
+let numeroA = 10;
+let numeroB = 20;
+let numeroC = 30;
+
+let soma = numeroA + numeroB + numeroC;
+console.log("A soma dos números é " + soma);
+
+let substracao = numeroC - numeroB
+console.log ("A subtração de: " + substracao);
+
+let multiplicacao = numeroC * numeroA;
+console.log("A multiplicação dos números é: " + multiplicacao);
+
+let divisao = numeroC / numeroA;
+console.log("A divisão dos números é: " + divisao);
