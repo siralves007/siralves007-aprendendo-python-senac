f9492939b5508d2f7e8bbae903e735bcf3ca91f1
Alves007 <seuemail@example.com>
2025-08-20 18:29:30 -0300

    atuando arquivo com atividades da aula

--- tee.js
+++ tee.js
@@ -1,1 +1,18 @@
 // testando novo arquivo para subir no git hub
+const prompt = require("prompt-sync")();
+// Início do código
+let somaNota = 0;
+for (let i = 0; i < 4; i++) {
+    let nota = parseFloat(prompt(`Digite a nota ${i + 1}: `));
+    somaNota += nota;
+}
+let media = somaNota / 4;
+console.log("Média: " + media);
+if (media >= 7) {
+    console.log("Aprovado");
+}else if (media >= 5) {
+    console.log("Recuperação"); 
+}else {
+    console.log("Reprovado");
+}
+// Fim do código
